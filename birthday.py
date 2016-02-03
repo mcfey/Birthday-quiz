820611ec9eeccdab24be32bfd379f520128bfab9
mcfey <maryfeyrer@hanovernorwichschools.org>
2016-02-03 13:33:38 -0500

    Updated from Brython Server: 2/3/2016 1:34:01 PM

--- birthday.py
+++ birthday.py
@@ -66,7 +66,7 @@ else:
         if birthyear >= 2000:
             decade = "two thousands"
         if birthyear >= 2016: 
-            decade = "the future" 
+            decade = "future" 
 
 print(name + ", you are a " + season + " baby of the " + decade+ ".")
 
